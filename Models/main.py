687bdff10c64f56b1ef74f3ae9980bb139b086bc
neerajac19 <neerajac22@gmail.com>
2025-03-28 16:34:48 +0530

    LSTM

--- Models/main.py
+++ Models/main.py
@@ -1,14 +1,20 @@
 
 from data_prep import data_processing
-from model_fitting import convol_neural
+from model_fitting import convol_neural, recurrent
 from data_upload import data_upload
+import pandas as pd
 
 
 
 if __name__ == "__main__":
-    signal = data_processing('/home/tranzmeo/Documents/ModelData')
-    signal.to_csv('signal.csv', index = False)
-    data_upload(signal)
-    convol_neural()
+    #signal = data_processing('/home/tranzmeo/Documents/ModelData')
+    signal = pd.read_csv(r'/home/tranzmeo/Learning/model creation/AiTraining/ModelData/INTERAMP_MHMBPL_TRAIN1.CSV', header = None)
+    signal.drop(columns=signal.columns[-1],  axis=1,  inplace=True)
+    #signal.to_csv('signal.csv', index = False)
+    signal = signal.loc[signal.iloc[:, -1].isin(['Manual Digging', 'Machine Digging', 'Vehicle Movement'])]
+    #data_upload(signal)
+    convol_neural(signal)
+    #recurrent(signal)
+
 
     
--- Models/model_fitting.py
+++ Models/model_fitting.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout
+from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout, LSTM
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 import matplotlib.pyplot as plt
@@ -37,8 +37,8 @@ def logistic(x_train, x_test, y_train, y_test):
 
 
 
-def convol_neural():
-    df = fetch_data()
+def convol_neural(df):
+    #df = fetch_data()
     X = df.iloc[:, :-1].values  
     y = df.iloc[:, -1].values   
 
@@ -85,6 +85,67 @@ def convol_neural():
 
     for i in range(5):
         print(f"Actual: {label_encoder.inverse_transform([y_test[i]])[0]}, Predicted: {y_pred_labels[i]}")
+
+
+
+
+def recurrent(df):
+    #df = fetch_data()
+
+    X = df.iloc[:, :-1].values  
+    y = df.iloc[:, -1].values   
+
+    label_encoder = LabelEncoder()
+    y = label_encoder.fit_transform(y)
+    num_classes = len(label_encoder.classes_)
+
+    y = y.reshape(-1, 1)
+
+    x = normalize(X, 'Minmax')
+
+    x = x.reshape((x.shape[0], x.shape[1], 1))
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+    model = Sequential()
+    model.add(LSTM(units=64, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=64))
+    model.add(Dropout(0.2))
+    #model.add(Dense(1, activation='sigmoid'))  # Ensure single neuron for binary classification and sigmoid activaton function
+
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    history = model.fit(x_train, y_train, epochs=20, batch_size=32, validation_data=(x_test, y_test))
+
+    plt.plot(history.history['accuracy'], label='Train Accuracy')
+    plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+    plt.legend()
+    plt.show()
+
+    test_loss, test_acc = model.evaluate(x_test, y_test)
+    print(f"Test Accuracy: {test_acc:.4f}")
+
+    y_pred = np.argmax(model.predict(x_test), axis=1)
+
+    y_pred_labels = label_encoder.inverse_transform(y_pred)
+
+    for i in range(5):
+        print(f"Actual: {label_encoder.inverse_transform([y_test[i]])[0]}, Predicted: {y_pred_labels[i]}")
+
+    # Predict and convert probabilities to binary values
+    #y_pred_prob = model.predict(x_test)
+    #y_pred = (y_pred_prob > 0.5).astype(int)  # Convert to binary labels (0 or 1)
+
+    # Inverse transform predictions
+    #y_pred_labels = label_encoder.inverse_transform(y_pred.flatten())
+    #y_test_actual = label_encoder.inverse_transform(y_test.flatten())
+
+    # Print some sample predictions
+    #for i in range(5):
+        #print(f"Actual: {y_test_actual[i]}, Predicted: {y_pred_labels[i]}")
 
 
 
